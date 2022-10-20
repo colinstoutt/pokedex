2b3a7ea098aa3916831203ebd16de4248ff96960
cps97 <colinstoutt@gmail.com>
2022-10-20 14:56:41 -0700

    add styles, add types svgs

---
+++ models/types.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    type: "bug",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Pok%C3%A9mon_Bug_Type_Icon.svg",
+  },
+  {
+    type: "dark",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg",
+  },
+  {
+    type: "dragon",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Pok%C3%A9mon_Dragon_Type_Icon.svg",
+  },
+  {
+    type: "electric",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg",
+  },
+  {
+    type: "fairy",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg",
+  },
+  {
+    type: "fighting",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg",
+  },
+  {
+    type: "fire",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/5/56/Pok%C3%A9mon_Fire_Type_Icon.svg",
+  },
+  {
+    type: "flying",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Pok%C3%A9mon_Flying_Type_Icon.svg",
+  },
+  {
+    type: "ghost",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Pok%C3%A9mon_Ghost_Type_Icon.svg",
+  },
+  {
+    type: "grass",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/f/f6/Pok%C3%A9mon_Grass_Type_Icon.svg",
+  },
+  {
+    type: "ground",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg",
+  },
+  {
+    type: "ice",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg",
+  },
+  {
+    type: "normal",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg",
+  },
+  {
+    type: "poison",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg",
+  },
+  {
+    type: "psychic",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg",
+  },
+  {
+    type: "rock",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg",
+  },
+  {
+    type: "steel",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg",
+  },
+  {
+    type: "water",
+    svg: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg",
+  },
+];
