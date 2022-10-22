58bdfd569ea9d551081ae07208e06e7f45100bbf
cps97 <colinstoutt@gmail.com>
2022-10-21 17:20:20 -0700

    add update route

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const PORT = 3000;
 // ===dependencies===
 const pokemon = require("./models/pokemon");
-const types = require("./models/types");
 const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
@@ -31,20 +30,34 @@ app.delete("/pokemon/:id", (req, res) => {
   res.redirect("/pokemon");
 });
 // ===update===
+app.put("/pokemon/:id", (req, res) => {
+  req.body.stats.hp = req.body.stats[0];
+  req.body.stats.attack = req.body.stats[1];
+  req.body.stats.defense = req.body.stats[2];
+  pokemon[req.params.id] = req.body;
+  res.redirect("/pokemon/:id");
+});
 // ===create===
-// CREATE route
 app.post("/pokemon/", (req, res) => {
-  console.log(req.body);
+  req.body.stats.hp = req.body.stats[0];
+  req.body.stats.attack = req.body.stats[1];
+  req.body.stats.defense = req.body.stats[2];
   pokemon.push(req.body);
   res.redirect("/pokemon");
 });
 // ===edit===
+app.get("/pokemon/:id/edit", (req, res) => {
+  res.render("edit.ejs", {
+    pokemon: pokemon[req.params.id],
+    index: req.params.id,
+    tabTitle: "| Edit",
+  });
+});
 // ===show===
 app.get("/pokemon/:id", (req, res) => {
   res.render("show.ejs", {
     pokemon: pokemon[req.params.id],
     index: req.params.id,
-    type: types,
     tabTitle: pokemon[req.params.id].name,
   });
 });
